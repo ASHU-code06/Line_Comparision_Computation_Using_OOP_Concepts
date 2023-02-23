763aa0fcc9dc21a4551645cab066319d334d9541
ASHU-code06 <ashuc306@gmail.com>
2023-02-24 00:33:04 +0530

    Checked if two lines are equal or not

--- com/linecomputeusingoop/LineComparision.java
+++ com/linecomputeusingoop/LineComparision.java
@@ -15,20 +15,34 @@ public class LineComparision {
         System.out.println("Enter x1 and y1 co-ordinate for line 1 ");
         double x1 = myinput.nextDouble();
         double y1 = myinput.nextDouble();
-        System.out.print("Entered value of x1 and y1 co-ordinates for line 1 are " +x1);
-        System.out.print(" and "+y1);
-        System.out.println(" respectively");
 
         System.out.println("Enter x2 and y2 co-ordinate for line 1 ");
         double x2 = myinput.nextDouble();
         double y2 = myinput.nextDouble();
 
-        System.out.print("Entered value of x2 and y2 co-ordinates for line 1 are " +x2);
-        System.out.print(" and "+y2);
-        System.out.println(" respectively");
-        System.out.println("Now let's calculate length of line 1" );
-        double len ;
-        len=Math.sqrt(Math.sqrt(Math.pow((x2-x1),2)+Math.pow((y2-y1),2))) ;
-        System.out.println("Length of line in cartesian system is  "+len+" units" );
+        double len1 ;
+        len1=Math.sqrt(Math.sqrt(Math.pow((x2-x1),2)+Math.pow((y2-y1),2)))  ;
+        System.out.println("length of line 1 is   "+len1);
+
+        System.out.println("Enter x1 and y1 co-ordinate for line 2 ");
+        double a1 = myinput.nextDouble();
+        double b1 = myinput.nextDouble();
+
+        System.out.println("Enter x2 and y2 co-ordinate for line 2 ");
+        double a2 = myinput.nextDouble();
+        double b2 = myinput.nextDouble();
+
+        double len2 ;
+        len2=Math.sqrt(Math.pow((a2-a1),2)+Math.pow((b2-b1),2)) ;
+        System.out.println("length of line 2 is   "+len2);
+
+        checkIfEquals(len1,len2);
+    }
+    void checkIfEquals(double len1 , double len2) {
+        if(len1==len2) {
+            System.out.println("The lines are equal");
+        }else {
+            System.out.println("The lines are not equal");
+        }
     }
 }
